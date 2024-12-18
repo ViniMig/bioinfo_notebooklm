84d7f41b1df481883e2ed53a947f0184f2d401ce
ViniMig <vinicio.m14@gmail.com>
2024-12-18 00:22:51 +0000

    add paper retrieval function and some initial structure

---
+++ src/retrieve_paper_bmc.py
@@ -0,0 +1,30 @@
+import requests
+
+import constants
+
+
+def retrieve_bmc_paper() -> str:
+    """Builds a query with personal API and a few settings
+    such as:
+      journal ISSN: for BMC Bioinformatics specifically;
+      MAX_RESULTS: corresponds to the 'p' setting in BMC API
+      START_RESULT_INDEX: corresponds to the 's' setting in BMC API
+
+    and retrieves the most recent json result (using API default behaviour,
+    by not setting date options in the query).
+
+    Returns the link to the full paper as a string. 
+    """
+    # Setup query parameters
+    BMC_KEY = constants.BMC_API_KEY
+    BMC_BIOINFORMATICS_ISSN = "1471-2105"
+    MAX_RESULTS = 1
+    START_RESULT_INDEX = 1
+
+    url = f'https://api.springernature.com/meta/v2/json?api_key={BMC_KEY}&callback=&s={START_RESULT_INDEX}&p={MAX_RESULTS}&q=(issn:{BMC_BIOINFORMATICS_ISSN})'
+    response = requests.request("GET", url)
+    result = response.json()
+
+    paper_url = url in result['records'][0]['url'][0]['value']
+
+    return paper_url
