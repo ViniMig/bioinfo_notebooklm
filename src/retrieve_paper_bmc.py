faf511eebc14619b173e60d31835bcea06e51fcf
ViniMig <vinicio.m14@gmail.com>
2024-12-18 15:55:22 +0000

    fix residual bug in getting url

--- src/retrieve_paper_bmc.py
+++ src/retrieve_paper_bmc.py
@@ -1,9 +1,9 @@
 import requests
 
-import constants
+from constants import BMC_API_KEY
 
 
-def retrieve_bmc_paper() -> str:
+def retrieve_bmc_paper():  # -> str:
     """Builds a query with personal API and a few settings
     such as:
       journal ISSN: for BMC Bioinformatics specifically;
@@ -16,15 +16,17 @@ def retrieve_bmc_paper() -> str:
     Returns the link to the full paper as a string. 
     """
     # Setup query parameters
-    BMC_KEY = constants.BMC_API_KEY
+    BMC_KEY = BMC_API_KEY
     BMC_BIOINFORMATICS_ISSN = "1471-2105"
     MAX_RESULTS = 1
     START_RESULT_INDEX = 1
 
-    url = f'https://api.springernature.com/meta/v2/json?api_key={BMC_KEY}&callback=&s={START_RESULT_INDEX}&p={MAX_RESULTS}&q=(issn:{BMC_BIOINFORMATICS_ISSN})'
+    url = f'https://api.springernature.com/meta/v2/json?api_key={BMC_KEY}&callback=&s={
+        START_RESULT_INDEX}&p={MAX_RESULTS}&q=(issn:{BMC_BIOINFORMATICS_ISSN})'
+
     response = requests.request("GET", url)
     result = response.json()
 
-    paper_url = url in result['records'][0]['url'][0]['value']
+    paper_url = result['records'][0]['url'][0]['value']
 
     return paper_url
